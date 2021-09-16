76c2ef2254c14b448cd3bfee028f5ef14a8fb6df
Rakshit Tiwari <rak1729e@gmail.com>
2021-09-17 00:55:10 +0530

    Added test cases for add rating and review modal

---
+++ src/__tests__/AddRatingModal/AddRatingModal.test.tsx
@@ -0,0 +1,32 @@
+import { shallow } from "enzyme";
+import AddRatingModal from "../../components/AddRating/AddRatingModal";
+import * as apiUtils from "../../components/AddRating/api";
+import GumroadButton from "../../components/Button/GumroadButton";
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"), // use actual for all non-hook parts
+  useParams: () => ({
+    id: {
+      id: "test",
+    },
+  }),
+}));
+
+describe("Add rating modal", () => {
+  it("should render the component", () => {
+    const wrapper = shallow(<AddRatingModal closeModal={jest.fn()} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should add rating and review and close the modal on submitting review", () => {
+    const handleClose = jest.fn();
+    const mockAddRatingAndReview = jest.spyOn(apiUtils, "addRatingAndReview");
+
+    const wrapper = shallow(<AddRatingModal closeModal={handleClose} />);
+    const button = wrapper.find(GumroadButton);
+    expect(button.exists()).toBeTruthy();
+    button.simulate("click");
+
+    expect(handleClose).toHaveBeenCalled();
+    expect(mockAddRatingAndReview).toHaveBeenCalled();
+  });
+});
--- src/__tests__/GumroadButton/GumroadButton.test.tsx
+++ src/__tests__/GumroadButton/GumroadButton.test.tsx
@@ -4,9 +4,15 @@ import GumroadButton from "../../components/Button/GumroadButton";
 
 describe("Gumroad button", () => {
   it("should render the component", () => {
-    const wrapper = shallow(
-      <GumroadButton onClick={jest.fn()}>test</GumroadButton>
-    );
+    const wrapper = shallow(<GumroadButton onClick={jest.fn()} />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should trigger the onClick handler on clicking button", () => {
+    const handleClick = jest.fn();
+    const wrapper = shallow(<GumroadButton onClick={handleClick} />);
+    wrapper.simulate("click");
+
+    expect(handleClick).toHaveBeenCalled();
   });
 });
